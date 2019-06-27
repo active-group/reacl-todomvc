7831b8cf6b363fc8cf46d9a4093b1f0f0beef2ee
David Frese <david.frese@active-group.de>
2019-06-27 18:49:02 +0200

    Fix onBlur of edit input.

--- src/todomvc/components/todo_edit.cljs
+++ src/todomvc/components/todo_edit.cljs
@@ -25,7 +25,7 @@
                 {:class "edit"
                  :ref input
                  :style {:display (helpers/display-elem editing)}
-                 :onblur (u/event-message this ->Blur)
+                 :onblur (reacl/return :message [this (->Blur)])
                  :onkeydown (u/event-message this ->KeyDown)})
 
   handle-message
